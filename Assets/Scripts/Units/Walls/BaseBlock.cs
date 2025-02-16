8d9904d55da1b36b33e547aa5ed2237ce146d8f5
EYoung21 <eliyoung4now@gmail.com>
2025-02-16 22:01:43 +0100

    fixed third prefab dragging to testblock1, was issue in class inheritence definition, renamed all instances of wall naming conventions to block

--- Assets/Scripts/Units/Walls/BaseBlock.cs
+++ Assets/Scripts/Units/Walls/BaseBlock.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class TestWall1 : MonoBehaviour
+public class BaseBlock : BaseUnit
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
--- Assets/Scripts/Units/Walls/TestBlock.cs
+++ Assets/Scripts/Units/Walls/TestBlock.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class BaseWall : BaseUnit
+public class TestWall1 : BaseBlock
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
