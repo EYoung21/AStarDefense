6b19f485de413b709d0ca0aa155442f2c5bc863d
Ethan K <eak2236@columbia.edu>
2025-02-20 05:40:01 -0500

    creating get world position

--- Assets/Scripts/Managers/Grid.cs
+++ Assets/Scripts/Managers/Grid.cs
@@ -4,17 +4,19 @@ public class Grid
 {
     private int width;
     private int height;
+    private float cellSize;
     private int[,] gridArray;
     public Grid(int width, int height)
     {
         this.width = width;
         this.height = height;
+        this.cellSize = cellSize;
 
         gridArray = new int[width, height];
     }
 
     private Vector3 GetWorldPositon(int x,  int y)
     {
-
+        return new Vector3(x, y) * cellSize;
     }
 }
