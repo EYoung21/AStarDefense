c5669604c504933ad571a0a41c35abf906eceda4
Ethan K <eak2236@columbia.edu>
2025-02-20 05:27:13 -0500

    logging tile format

--- Assets/Scripts/Managers/GridManager.cs
+++ Assets/Scripts/Managers/GridManager.cs
@@ -51,7 +51,10 @@ public class GridManager : MonoBehaviour
         
         GameManager.Instance.ChangeState(GameState.SpawnInitialTurret);
 
-        Debug.Log(_tiles);
+        foreach (var tile in _tiles)
+        {
+            Debug.Log($"Key: {tile.Key}, Tile Name: {tile.Value.name}");
+        }
     }
     
     public Tile GetTileAtPosition(Vector2 pos) {
