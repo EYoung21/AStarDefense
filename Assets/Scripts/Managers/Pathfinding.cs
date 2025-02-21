1be61d090ee54149c006d679e59778dc90ed899a
Ethan K <eak2236@columbia.edu>
2025-02-21 05:58:19 -0500

    creating pathfinding script and pathnode script

--- Assets/Scripts/Managers/Pathfinding.cs
+++ Assets/Scripts/Managers/Pathfinding.cs
@@ -2,6 +2,9 @@ using UnityEngine;
 
 public class Pathfinding
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public Pathfinding(int width, int height)
+    {
+
+    }
     
 }
