43891f51ac2afe5a9dc50a01b433c3204c3fe2e5
Ethan K <eak2236@columbia.edu>
2025-02-21 06:46:25 -0500

    defining pathnode class

--- Assets/Scripts/Managers/Pathnode.cs
+++ Assets/Scripts/Managers/Pathnode.cs
@@ -5,6 +5,11 @@ public class PathNode
     private Grid<PathNode> grid;
     private int x;
     private int y;
+
+    public int gCost;
+    public int fCost;
+    public int hCost;
+
     public PathNode(Grid<PathNode> grid, int x, int y)
     {
         this.grid = grid;
