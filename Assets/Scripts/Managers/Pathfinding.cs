33d63f4b9b14a7d454d21d033775e0dc9a2d76b4
Ethan K <eak2236@columbia.edu>
2025-02-25 05:22:51 -0500

    adding findPath method

--- Assets/Scripts/Managers/Pathfinding.cs
+++ Assets/Scripts/Managers/Pathfinding.cs
@@ -14,6 +14,7 @@ public class Pathfinding
 
     private List<PathNode> FindPath(int startX, int startY, int endX, int endY)
     {
+        PathNode startNode = grid.GetGridObject(startX, startY);
         openList = new List<PathNode>();
         closedList = new List<PathNode>();
     }
