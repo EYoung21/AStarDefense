7b49a77d6b84cba348a053df763f395486fd8ec9
Ethan K <eak2236@columbia.edu>
2025-02-25 05:21:15 -0500

    adding findPath method

--- Assets/Scripts/Managers/Pathfinding.cs
+++ Assets/Scripts/Managers/Pathfinding.cs
@@ -1,3 +1,5 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Pathfinding
@@ -6,6 +8,11 @@ public class Pathfinding
     public Pathfinding(int width, int height)
     {
         grid = new Grid<PathNode>(width, height, 1f, Vector3.zero, (Grid<PathNode> g, int x, int y) => new PathNode(g,x,y));
+    }
+
+    private List<PathNode> FindPath(int startX, int startY, int endX, int endY)
+    {
+
     }
     
 }
