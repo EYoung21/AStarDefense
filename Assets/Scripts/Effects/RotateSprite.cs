0dd7132da5b5d029cebacd486e7222f4a2f0ac67
Ethan K <eak2236@columbia.edu>
2025-03-02 08:48:49 -0500

    changing star animation

--- Assets/Scripts/Effects/RotateSprite.cs
+++ Assets/Scripts/Effects/RotateSprite.cs
@@ -2,15 +2,10 @@ using UnityEngine;
 
 public class RotateSprite : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    public float rotationSpeed = 100f; // Adjust this value to control speed
 
-    // Update is called once per frame
     void Update()
     {
-        
+        transform.Rotate(Vector3.forward * rotationSpeed * Time.deltaTime);
     }
 }
