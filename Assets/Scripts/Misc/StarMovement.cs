502edc6a4d6802141faf10967bde0dfafab15cab
Eli Young <eliyoung4now@gmail.com>
2025-03-04 13:13:07 +0100

    fixed commenting conventions

--- Assets/Scripts/Misc/StarMovement.cs
+++ Assets/Scripts/Misc/StarMovement.cs
@@ -9,12 +9,6 @@ public class StarMovement : MonoBehaviour
         //move diagonally down-left
         transform.position += new Vector3(-1, -1, 0) * speed * Time.deltaTime;
         
-        // Debug every few seconds
-        if (Time.frameCount % 60 == 0)
-        {
-            Debug.Log($"Star position: {transform.position}");
-        }
-        
         // Destroy stars once they're well off-screen
         Vector2 screenBottomLeft = Camera.main.ViewportToWorldPoint(new Vector2(0, 0));
         if (transform.position.x < screenBottomLeft.x - 2f || transform.position.y < screenBottomLeft.y - 2f)
